ea3bfaee089c84f8b0eedd9f9b8fdb2c42a0d8c1
Bryan <bryanrtboy@users.noreply.github.com>
2021-08-25 14:51:57 -0600

    Initial Script

--- Assets/MemeMaker/Scripts/MakerScript.cs
+++ Assets/MemeMaker/Scripts/MakerScript.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MakerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public RawImage[] m_images;
+    public string[] m_paths = new string[]{"Top", "MiddleTop","MiddleBottom", "Bottom"};
+    public Object[] textures;
+    public Texture2D texture;
+    private GameObject go;
+
     void Start()
     {
-        
-    }
+        textures = Resources.LoadAll(m_paths[0], typeof(Texture2D));
 
-    // Update is called once per frame
-    void Update()
+        foreach (var t in textures)
+        {
+            Debug.Log(t.name);
+        }
+
+        go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    }
+    
+    public void GenerateRandomCharacter()
     {
-        
+        int i = Random.Range(0, textures.Length + 1);
+        i -= 1;
+        if (i >= 0)
+        {
+            texture = (Texture2D) textures[i];
+        }
+        else
+        {
+            texture = null;
+        }
     }
 }
